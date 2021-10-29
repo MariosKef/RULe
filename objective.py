7c360c26c456f873837036351c0d2898615e3e1e
Bas van Stein <neocortex@neocortex.liacs.nl>
2021-10-29 13:06:17 +0000

    Fixed groth for just 1 device

--- objective.py
+++ objective.py
@@ -15,9 +15,6 @@ import sys
 import tensorflow as tf
 tf.random.set_seed(42)
 
-physical_devices = tf.config.list_physical_devices('GPU') 
-for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
 
 # sklearn
 from sklearn import pipeline
@@ -253,5 +250,8 @@ if len(sys.argv) > 2 and sys.argv[1] == '--cfg':
         
         os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
         os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu)
+        physical_devices = tf.config.list_physical_devices('GPU') 
+        for device in physical_devices:
+            tf.config.experimental.set_memory_growth(device, True)
     print(obj_function(cfg, None))
     k.clear_session()
