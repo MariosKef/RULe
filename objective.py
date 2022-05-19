b0d492ec773698e91c8d1513a88a1cc221817990
M. Kefalas <kefalasm@neocortex.liacs.nl>
2022-05-19 08:40:28 +0000

    Paper prep: updated objective.py, tests_dataset1.ipynb, tests_dataset3.ipynb.

--- objective.py
+++ objective.py
@@ -16,7 +16,6 @@ import sys
 # tensorflow
 import tensorflow as tf
 
-# fix for reproducibility
 tf.random.set_seed(42)
 
 
@@ -24,6 +23,7 @@ tf.random.set_seed(42)
 from sklearn import pipeline
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import backend as k
 
@@ -31,6 +31,7 @@ from tensorflow.keras import backend as k
 from data import load_data
 from modeling import network
 from preprocessing import build_data
+import GPUtil as gp
 
 
 def weibull_mean(alpha, beta):
@@ -65,11 +66,12 @@ def obj_function(net_cfg, cfg=None):
         test_x_orig,
         test_y_orig,
     ) = load_data()
-
+    
     # Uncomment for debugging
     # print(train_x_orig.shape)
     # print(vld_trunc.shape)
     # print(original_len.shape)
+
 
     rmse_train = []
     r2_train = []
@@ -84,7 +86,7 @@ def obj_function(net_cfg, cfg=None):
     train_all = []
     test_all = []
 
-    file = "results_no_cv_HO_28_1"
+    file = "new_results_single_obj_dataset_1_2_3_retake"
     columns = [
         "rmse_train",
         "mae_train",
@@ -144,18 +146,24 @@ def obj_function(net_cfg, cfg=None):
 
     # Uncomment for debugging
     # print(
-    #     "train_x",
-    #     train_x.shape,
-    #     "train_y",
-    #     train_y.shape,
-    #     "test_x",
-    #     test_x.shape,
-    #     "test_y",
-    #     test_y.shape,
+    #    "train_x",
+    #    train_x.shape,
+    #    "train_y",
+    #    train_y.shape,
+    #    "test_x",
+    #    test_x.shape,
+    #    "test_y",
+    #    test_y.shape,
     # )
 
     # training
     model, history = network(train_x, train_y, test_x, test_y, net_cfg, cfg)
+
+    # For debugging
+    # plt.plot(history.history['loss'], label='training')
+    # plt.plot(history.history['val_loss'], label='validation')
+    # plt.title('loss')
+    # plt.legend()
 
     # predicting the rul on the train fold
     train_predict_1 = []
@@ -173,13 +181,13 @@ def obj_function(net_cfg, cfg=None):
                 train_predict[:, 1].reshape(train_predict[:, 1].shape[0], 1)
             )
 
-        train_predict_1_median = np.median(np.hstack(train_predict_1), axis=1)
-        train_predict_2_median = np.median(np.hstack(train_predict_2), axis=1)
-        train_predict_1_median = train_predict_1_median.reshape(
-            train_predict_1_median.shape[0], 1
+        train_predict_1_mean = np.median(np.hstack(train_predict_1), axis=1)
+        train_predict_2_mean = np.median(np.hstack(train_predict_2), axis=1)
+        train_predict_1_mean = train_predict_1_mean.reshape(
+            train_predict_1_mean.shape[0], 1
         )
-        train_predict_2_median = train_predict_2_median.reshape(
-            train_predict_2_median.shape[0], 1
+        train_predict_2_mean = train_predict_2_mean.reshape(
+            train_predict_2_mean.shape[0], 1
         )
         train_predict_1_std = np.std(np.hstack(train_predict_1), axis=1)
         train_predict_2_std = np.std(np.hstack(train_predict_2), axis=1)
@@ -192,8 +200,8 @@ def obj_function(net_cfg, cfg=None):
 
         train_predict = np.hstack(
             [
-                train_predict_1_median,
-                train_predict_2_median,
+                train_predict_1_mean,
+                train_predict_2_mean,
                 train_predict_1_std,
                 train_predict_2_std,
             ]
@@ -203,13 +211,13 @@ def obj_function(net_cfg, cfg=None):
         train_result = np.concatenate((train_y, train_predict), axis=1)
         train_results_df = pd.DataFrame(
             train_result,
-            columns=["T", "median_alpha", "median_beta", "std_alpha", "std_beta"],
+            columns=["T", "mean_alpha", "mean_beta", "std_alpha", "std_beta"],
         )
         train_results_df["unit_number"] = train_x_orig["unit_number"].to_numpy()
         train_results_df["time"] = train_x_orig["time"].to_numpy()
 
         train_results_df["predicted_mu"] = train_results_df[
-            ["median_alpha", "median_beta"]
+            ["mean_alpha", "mean_beta"]
         ].apply(lambda row: weibull_mean(row[0], row[1]), axis=1)
         train_results_df["uncertainty"] = np.mean(train_predict[:, 2:], axis=1)
 
@@ -226,13 +234,13 @@ def obj_function(net_cfg, cfg=None):
                 test_predict[:, 1].reshape(test_predict[:, 1].shape[0], 1)
             )
 
-        test_predict_1_median = np.median(np.hstack(test_predict_1), axis=1)
-        test_predict_2_median = np.median(np.hstack(test_predict_2), axis=1)
-        test_predict_1_median = test_predict_1_median.reshape(
-            test_predict_1_median.shape[0], 1
+        test_predict_1_mean = np.median(np.hstack(test_predict_1), axis=1)
+        test_predict_2_mean = np.median(np.hstack(test_predict_2), axis=1)
+        test_predict_1_mean = test_predict_1_mean.reshape(
+            test_predict_1_mean.shape[0], 1
         )
-        test_predict_2_median = test_predict_2_median.reshape(
-            test_predict_2_median.shape[0], 1
+        test_predict_2_mean = test_predict_2_mean.reshape(
+            test_predict_2_mean.shape[0], 1
         )
         test_predict_1_std = np.std(np.hstack(test_predict_1), axis=1)
         test_predict_2_std = np.std(np.hstack(test_predict_2), axis=1)
@@ -241,22 +249,24 @@ def obj_function(net_cfg, cfg=None):
 
         test_predict = np.hstack(
             [
-                test_predict_1_median,
-                test_predict_2_median,
+                test_predict_1_mean,
+                test_predict_2_mean,
                 test_predict_1_std,
                 test_predict_2_std,
             ]
         )
 
-        test_predict = np.resize(test_predict, (test_x.shape[0], 4))
+        test_predict = np.resize(
+            test_predict, (test_x.shape[0], 4)
+        )  # changed from 2 to 4
         test_result = np.concatenate((test_y, test_predict), axis=1)
         test_results_df = pd.DataFrame(
             test_result,
-            columns=["T", "median_alpha", "median_beta", "std_alpha", "std_beta"],
+            columns=["T", "mean_alpha", "mean_beta", "std_alpha", "std_beta"],
         )
 
         test_results_df["predicted_mu"] = test_results_df[
-            ["median_alpha", "median_beta"]
+            ["mean_alpha", "mean_beta"]
         ].apply(lambda row: weibull_mean(row[0], row[1]), axis=1)
         test_results_df["uncertainty"] = np.mean(test_predict[:, 2:], axis=1)
 
@@ -306,7 +316,7 @@ def obj_function(net_cfg, cfg=None):
 
     if success == False:
         print("Failed")
-        return 1e4, 1e4, False  # not successful
+        return 1e4  # not successful
 
     # registering results
     results["rmse_train"] = rmse_train
@@ -319,21 +329,39 @@ def obj_function(net_cfg, cfg=None):
     results["uncertainty_test"] = std_test
     results["net_cfg"] = json.dumps(net_cfg)
 
-    # Uncomment for debugging
-    print(results)
+    x = results["rmse_test"]
+    y = results["rmse_test"].mean()
+    z = rmse_test
 
+    # Uncomment for debugging
+    #  print(results)
+
+    # return (
+    #     model,
+    #     train_results_df,
+    #     test_results_df,
+    #     test_x_orig,
+    #     test_y_orig,
+    #     scaler,
+    #     train_x,
+    #     test_x,
+    # )
+    
     # Saving results
     if os.path.isfile(file):
         results.to_csv("./" + file, mode="a", index=False, header=False)
     else:
         results.to_csv("./" + file, mode="w", index=False, header=True)
 
-    if np.isfinite(results["rmse_test"].mean()) and np.isfinite(
-        results["uncertainty_test"].mean()
+    if np.isfinite(results["rmse_test"].to_numpy()) and np.isfinite(
+        results["uncertainty_test"].to_numpy()
     ):
-        return results["rmse_test"].mean(), results["uncertainty_test"].mean(), True
+        return 2 / (
+            (1 / results["rmse_test"].to_numpy())
+            + (1 / results["uncertainty_test"].to_numpy())
+        )
     else:
-        return 1e4, 1e4, False  # not successful
+        return 1e4
 
 
 # system arguments (configuration)
@@ -342,10 +370,12 @@ if len(sys.argv) > 2 and sys.argv[1] == "--cfg":
     if len(sys.argv) > 3:
         gpu = sys.argv[3]
 
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
-        physical_devices = tf.config.list_physical_devices("GPU")
-        for device in physical_devices:
-            tf.config.experimental.set_memory_growth(device, True)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+    print(f"GPU: {gpu} on cfg:{cfg}")
+    physical_devices = tf.config.list_physical_devices("GPU")
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
     print(obj_function(cfg, None))
     k.clear_session()
